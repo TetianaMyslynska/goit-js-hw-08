d0d5029d7ad7b673b430cfe970c1b21bf3244b48
TetianaMyslynska <tetmyslynska@gmail.com>
2022-02-09 20:11:18 +0100

    task1-library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const elements = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href='${original}'>
+  <img class="gallery__image" src='${preview}' alt='${description}' />
+</a>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', elements);
+galleryRef.addEventListener('click', onElementClick);
+
+function onElementClick(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  evt.preventDefault();
+  var lightbox = new SimpleLightbox('.gallery a', {
+    nav: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+  });
+}
